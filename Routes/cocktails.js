b04f18ecb617bcde999a91d8ffcb386b3bb0ab84
DanielVoric <voricdaniel@gmail.com>
2023-08-20 16:24:41 +0200

    Dodana delete mogucnost

--- Routes/cocktails.js
+++ Routes/cocktails.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const cocktail = require('../models/Cocktail');
+const Cocktail = require('../models/Cocktail'); 
 
 router.get('/', async (req, res) => {
-    const cocktails = await cocktail.find({});
-    res.json(cocktails);
+    try {
+        const cocktails = await Cocktail.find({});
+        res.json(cocktails);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to fetch cocktails.", error });
+    }
 });
 
-// sranje
 router.get('/search', async (req, res) => {
     const { alcohol, juice, other } = req.query;
 
@@ -17,32 +20,46 @@ router.get('/search', async (req, res) => {
         ...other ? other.split(",").map(oth => oth.toLowerCase().trim()) : []
     ];
 
-    // Dohvati sve
-    const allCocktails = await cocktail.find({});
+    try {
+        const allCocktails = await Cocktail.find({});
+        const matchingCocktails = allCocktails.filter(cocktail => {
+            const totalIngredients = [
+                ...cocktail.ingredients.alcohol.map(ing => ing.toLowerCase().trim()),
+                ...cocktail.ingredients.juice.map(ing => ing.toLowerCase().trim()),
+                ...cocktail.ingredients.other.map(ing => ing.toLowerCase().trim())
+            ];
 
-    const matchingCocktails = allCocktails.filter(cocktail => {
-        const totalIngredients = [
-            ...cocktail.ingredients.alcohol.map(ing => ing.toLowerCase().trim()),
-            ...cocktail.ingredients.juice.map(ing => ing.toLowerCase().trim()),
-            ...cocktail.ingredients.other.map(ing => ing.toLowerCase().trim())
-        ];
+            return totalIngredients.every(ing => selectedIngredients.includes(ing));
+        });
 
-        // Filtriraj
-        return totalIngredients.every(ing => selectedIngredients.includes(ing));
-    });
-
-    res.json(matchingCocktails);
+        res.json(matchingCocktails);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to search cocktails.", error });
+    }
 });
 
 router.post('/', async (req, res) => {
     const cocktailData = req.body;
-    const newCocktail = new cocktail(cocktailData);
+    const newCocktail = new Cocktail(cocktailData);
 
     try {
         await newCocktail.save();
         res.status(201).json({ message: "Cocktail added!", cocktail: newCocktail });
     } catch (error) {
         res.status(400).json({ message: "Failed to add cocktail.", error });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedCocktail = await Cocktail.findByIdAndRemove(req.params.id);
+        if (!deletedCocktail) {
+            return res.status(404).json({ message: "Cocktail not found!" });
+        }
+
+        res.status(200).json({ message: "Cocktail deleted successfully!", deletedCocktail });
+    } catch (error) {
+        res.status(500).json({ message: "Failed to delete cocktail.", error });
     }
 });
 
