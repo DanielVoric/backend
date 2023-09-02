f9651cd1c023402192e4e35653d068a89e73c6db
DanielVoric <voricdaniel@gmail.com>
2023-09-02 02:23:17 +0200

    natrag na vercel

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleman
-app.use(cors({
-    origin: 'https://koktelomat-ztvt.onrender.com',
-    credentials: true,
-  }));
+app.use(cors());
   app.use(express.json());
 
 
